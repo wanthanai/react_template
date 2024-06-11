4c3c8721841801e195aeb2804094c105bd593a24
wanthanai kaiphet <wanthanai.bass@nilecon.com>
2024-06-11 10:41:25 +0700

    publish template

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../components/Header'
+import './style.css'
+
+function Home() {
+  return (
+    <div className='home-container'>
+        <Header />
+        <main>
+            Home Content
+        </main>
+    </div>
+  )
+}
+
+export default Home
